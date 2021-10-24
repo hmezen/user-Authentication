7c04e1fe8130e2ccbe561e96f4e6e5a6ce88c1e6
mezen <mezen.hamdeni@vistaprint.com>
2021-10-24 21:09:38 +0100

    update dialog style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -60,10 +60,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   appBarSpacer: theme.mixins.toolbar,
-  form: {
-    // width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
+
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
